a196e5381f1ea18d67b59aae002b2dcc7585a92d
zarthjr <zarthjr@gmail.com>
2017-01-23 15:22:18 -0200

    Criada conexão com o banco
    Close #1

---
+++ POCO/erro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace issConfigReader.POCO
+{
+    public class erro
+    {
+        public string mensagem { get; set; }
+        public string nomeSite { get; set; }
+        public string aplicacao { get; set; }
+        public DateTime data { get; set; }
+    }
+}
